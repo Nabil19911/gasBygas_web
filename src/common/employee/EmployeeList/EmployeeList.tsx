a315700f2cbe39004f8b044b72a03ff5e8a8535a
dewanthika <dewanthika@gmail.com>
2025-01-31 15:43:33 +0530

    employee form, employee list, employee row implemented

--- src/common/employee/EmployeeList/EmployeeList.tsx
+++ src/common/employee/EmployeeList/EmployeeList.tsx
@@ -1,12 +1,36 @@
 // TDOD: this compnenet needs to fix
+import { useCallback } from "react";
 import IEmployee from "../../../type/IEmployee";
+import Table from "../../ui-components/table";
+import EmployeeRow from "./EmployeeRow";
 
 interface IEmployeeListProps {
   employees?: IEmployee[];
 }
 
 const EmployeeList = ({ employees }: IEmployeeListProps) => {
-  return <div></div>;
+  const renderEmployeeRow = useCallback(() => {
+    if (employees) {
+      return employees.map((user) => (
+        <EmployeeRow key={user._id} user={user} />
+      ));
+    }
+    return [];
+  }, [employees]);
+
+  return (
+    <Table>
+      <Table.Header>
+        <Table.Column>First name</Table.Column>
+        <Table.Column>Email</Table.Column>
+        <Table.Column>Contact</Table.Column>
+        <Table.Column>Role</Table.Column>
+        <Table.Column>Status</Table.Column>
+        <Table.Column>Action</Table.Column>
+      </Table.Header>
+      <Table.Body>{renderEmployeeRow()}</Table.Body>
+    </Table>
+  );
 };
 
 export default EmployeeList;
--- src/common/employee/EmployeeList/EmployeeRow/EmployeeRow.tsx
+++ src/common/employee/EmployeeList/EmployeeRow/EmployeeRow.tsx
@@ -1,11 +1,25 @@
 import IEmployee from "../../../../type/IEmployee";
+import Table from "../../../ui-components/table";
 
 interface IEmployeeRowProps {
   user: IEmployee;
 }
 
 const EmployeeRow = ({ user }: IEmployeeRowProps) => {
-  return <div></div>;
+  return (
+    <Table.Row key={user.email}>
+      <Table.Cell>
+        <span className="font-medium text-gray-900">
+          {user?.first_name ?? "--"}
+        </span>
+      </Table.Cell>
+      <Table.Cell>{user?.email ?? "--"}</Table.Cell>
+      <Table.Cell>{user?.contact ?? "--"}</Table.Cell>
+      <Table.Cell>{user?.role ?? "--"}</Table.Cell>
+      <Table.Cell>{user?.status ?? "--"}</Table.Cell>
+      <Table.Cell>Action</Table.Cell>
+    </Table.Row>
+  );
 };
 
 export default EmployeeRow;
